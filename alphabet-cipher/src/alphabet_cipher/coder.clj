005a1be796a4ab0c24df918f3420abfa93a2ab0c
Jonas Jebing <noreply.jonas.8049@protonmail.com>
2020-11-01 16:08:47 +0100

    implemented alphabet-cipher encode and decode

--- alphabet-cipher/src/alphabet_cipher/coder.clj
+++ alphabet-cipher/src/alphabet_cipher/coder.clj
@@ -1,11 +1,39 @@
 (ns alphabet-cipher.coder)
 
+(defn zip [& collections]
+  (apply map vector collections))
+
+(defn char-range [incl-start incl-end]
+  (let [incl-start (int incl-start)
+        excl-end (inc (int incl-end))]
+    (map char (range incl-start excl-end))))
+
+(defn shift
+  ([collection] (concat (rest collection) [(first collection)]))
+  ([n collection] (concat (drop n collection) (take n collection))))
+
+(def alphabet (char-range \a \z))
+
+(def shifted-alphabets
+  (map-indexed shift (repeat (count alphabet) alphabet)))
+
+(defn index-of [coll item]
+  (count (take-while #(not= item %) coll)))
+
+(defn encode-char [key-char message-char]
+  (let [key-index (index-of alphabet key-char)
+        message-index (index-of alphabet message-char)]
+    (nth (nth shifted-alphabets key-index) message-index)))
+
+(defn decode-char [key-char message-char]
+  (let [key-index (index-of alphabet key-char)]
+    (nth alphabet (index-of (nth shifted-alphabets key-index) message-char))))
+
 (defn encode [keyword message]
-  "encodeme")
+  (apply str (map encode-char (cycle keyword) message)))
 
 (defn decode [keyword message]
-  "decodeme")
+  (apply str (map decode-char (cycle keyword) message)))
 
 (defn decipher [cipher message]
   "decypherme")
-
--- alphabet-cipher/test/alphabet_cipher/coder_test.clj
+++ alphabet-cipher/test/alphabet_cipher/coder_test.clj
@@ -2,12 +2,61 @@
   (:require [clojure.test :refer :all]
             [alphabet-cipher.coder :refer :all]))
 
+(deftest test-zip
+  (testing "can zip 2 sequences together"
+    (is (= [[1 4] [2 5] [3 6]]
+           (zip [1 2 3 9] [4 5 6])))))
+
+(deftest test-char-range
+  (testing "char-range a d"
+    (is (= [\a \b \c \d]
+           (char-range \a \d)))))
+
+(deftest test-shift
+  (testing "can shift an int sequence"
+    (is (= [2 3 4 1]
+           (shift [1 2 3 4]))))
+  (testing "can shift an int sequence by n"
+    (is (= [3 4 5 1 2]
+           (shift 2 [1 2 3 4 5])))))
+
+(deftest test-alphabet
+  (testing "alphabet starts with abc"
+    (is (= [\a \b \c]
+           (take 3 alphabet)))))
+
+(deftest test-shifted-alphabet
+  (testing "shifted-alphabets starts with alphabet and alphabet shifted by 1"
+    (is (= [alphabet (shift alphabet)]
+           (take 2 shifted-alphabets))))
+  (testing "shifted-alphabets ends with alphabet shifted by 25"
+    (is (= (shift 25 alphabet)
+           (last shifted-alphabets)))))
+
+(deftest test-index-of
+  (testing "index-of 3rd element is 2"
+    (is (= 2
+           (index-of [\a \b \c \d \e] \c)))))
+
+(deftest test-encode-char
+  (testing "m encoded with v is h"
+    (is (= \h
+           (encode-char \v \m)))))
+
 (deftest test-encode
   (testing "can encode a message with a secret keyword"
     (is (= "hmkbxebpxpmyllyrxiiqtoltfgzzv"
            (encode "vigilance" "meetmeontuesdayeveningatseven")))
     (is (= "egsgqwtahuiljgs"
            (encode "scones" "meetmebythetree")))))
+
+(deftest test-decode-char
+  (testing "e decoded with s is m"
+    (is (= \m
+           (decode-char \s \e))))
+  (testing "h decoded with v is m"
+    (is (= \m
+           (decode-char \v \h)))))
 
 (deftest test-decode
   (testing "can decode a message given an encoded message and a secret keyword"
